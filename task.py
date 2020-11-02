14c82b2491c0812247c5b1c88758003b09823290
repl.it user <replituser@example.com>
2020-11-02 15:18:40 +0000

    done

--- task.py
+++ task.py
@@ -1,24 +1,31 @@
-#
-#Flight simulator. 
-#Write a code in python that simulates the tilt correction of the plane (angle between plane wings and earth). 
-#The program should print out current orientation, and applied tilt correction.
-# (Tilt is "Roll" in this diagram https://www.novatel.com/assets/Web-Phase-2-2012/Solution-Pages/AttitudePlane.png)
-#The program should run in infinite loop, until user breaks the loop. 
-#Assume that plane orientation in every new simulation step is changing with random angle with gaussian distribution (the planes is experiencing "turbuence"). 
-# Hint: "random.gauss(0, 2*rate_of_correction)"
-#With every simulation step the orentation should be corrected, correction should be applied and printed out.
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#
-#Try to expand your implementation as best as you can. 
-#Think of as many features as you can, and try implementing them.
-#Make intelligent use of pythons syntactic sugar (overloading, iterators, generators, etc)
-#Most of all: CREATE GOOD, RELIABLE, READABLE CODE.
-#The goal of this task is for you to SHOW YOUR BEST python programming skills.
-#Impress everyone with your skills, show off with your code.
-#
-#Your program must be runnable with command "python task.py".
-#Show some usecases of your library in the code (print some things)
-#Delete these comments before commit!
-#
-#Good luck.
+import random
+
+def GetRandomRate():
+  return random.randrange(0,360)
+
+def GetRandomAngle():
+  rate_of_correction = GetRandomRate()
+  return random.gauss(0, 2*rate_of_correction)
+
+if __name__ == "__main__":
+
+  roll_orientation = 0
+  pitch_orientation = 0
+  yaw_orientation = 0
+
+  print("Roll orientation: {}, pitch orientation: {}, yaw orientation: {}. \n". format(roll_orientation,pitch_orientation,yaw_orientation))
+
+  while(True):
+    roll_angle = GetRandomAngle()
+    pitch_angle = GetRandomAngle()
+    yaw_angle = GetRandomAngle()
+
+    roll_orientation += roll_angle
+    pitch_orientation += pitch_angle
+    yaw_orientation += yaw_angle
+
+   
+
+    print("Roll orientation: {}, pitch orientation: {}, yaw orientation: {}. \n". format(roll_orientation,pitch_orientation,yaw_orientation))
+
+    
