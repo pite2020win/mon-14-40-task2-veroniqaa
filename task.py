9fd2f972aac7168663d699b536d51b9c318a5cb6
Weronika <65810449+veroniqaa@users.noreply.github.com>
2020-11-15 17:56:34 +0100

    Update task.py

--- task.py
+++ task.py
@@ -1,31 +1,77 @@
+from abc import ABC, abstractmethod
 import random
+import logging
 
-def GetRandomRate():
-  return random.randrange(0,360)
+logging.basicConfig(level=logging.INFO, format='%(message)s')
 
-def GetRandomAngle():
-  rate_of_correction = GetRandomRate()
-  return random.gauss(0, 2*rate_of_correction)
+class Event:
 
-if __name__ == "__main__":
+	@abstractmethod
+	def modify(self, plane):
+		pass
 
-  roll_orientation = 0
-  pitch_orientation = 0
-  yaw_orientation = 0
+class Turbulence(Event):
 
-  print("Roll orientation: {}, pitch orientation: {}, yaw orientation: {}. \n". format(roll_orientation,pitch_orientation,yaw_orientation))
+	def __init__(self, mu, sigma):
+		self.mu = mu
+		self.sigma = sigma
 
-  while(True):
-    roll_angle = GetRandomAngle()
-    pitch_angle = GetRandomAngle()
-    yaw_angle = GetRandomAngle()
+	def modify(self, plane):
+		plane.yaw += random.gauss(self.mu, self.sigma)
+		plane.roll += random.gauss(self.mu, self.sigma)
+		plane.pitch += random.gauss(self.mu, self.sigma)
 
-    roll_orientation += roll_angle
-    pitch_orientation += pitch_angle
-    yaw_orientation += yaw_angle
+class Correction(Event):
 
-   
+	def __init__(self, rtc, mu, sigma):
+		self.rate_of_correction = rtc
+		self.mu = mu
+		self.sigma = sigma
 
-    print("Roll orientation: {}, pitch orientation: {}, yaw orientation: {}. \n". format(roll_orientation,pitch_orientation,yaw_orientation))
+	def modify(self, plane):	
+		plane.roll -= random.gauss(self.mu, self.sigma*self.rate_of_correction)
+		plane.pitch -= random.gauss(self.mu, self.sigma*self.rate_of_correction)
+		plane.yaw -= random.gauss(self.mu, self.sigma*self.rate_of_correction)
 
-    
+class Plane:
+	def __init__ (self, roll, pitch, yaw):
+		self.roll = roll
+		self.pitch = pitch
+		self.yaw = yaw
+
+	def simulate_correction(self):
+		cor = Correction(2, 1, 2)
+		cor.modify(self)
+
+	def take_values(self):
+		values=[self.roll, self.pitch, self.yaw]
+		return values
+	
+class Environment:
+	def __init__ (self):
+		self.planes=[]
+
+	def add_plane(self, plane):
+		self.planes.append(plane)
+
+	def simulate_turbulance(self):
+		turb = Turbulence(1,4)
+		for p in self.planes:
+			turb.modify(p)
+
+if __name__ == '__main__':
+
+	plane1 = Plane(0,0,0)
+	env = Environment()
+	env.add_plane(plane1)
+
+	i = 1
+
+	while (i<30):
+		env.simulate_turbulance()
+		val = plane1.take_values()	
+		logging.info("roll = {}, pitch = {}, yaw = {} \n".format(val[0], val[1], val[2]))	
+		
+		plane1.simulate_correction()
+
+		i+=1
